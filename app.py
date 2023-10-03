4cd1e995a8b37accb2f5f7271d495e3d9b5cc130
Dinorah Garcia Vasquez <83092931+DinorahGV02@users.noreply.github.com>
2023-10-02 22:54:22 -0400

    Homework Finished! tada

--- app.py
+++ app.py
@@ -4,17 +4,14 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('baseball.csv')
 
-# Select necessary columns and set the 'Team' column as the index
-plot_data = data.set_index('Team')[['RS', 'RA']]
-
-# Create a dropdown menu for year selection
+# Dropdown menu
 selected_year = st.selectbox('Select Year', data['Year'].unique())
 
-# Filter data based on selected year
+# Filter with year
 filtered_data = data[data['Year'] == selected_year]
 
-# Create a bar chart for Runs Scored
-rs_chart_data = filtered_data.sort_values('RS', ascending=False)
+# Runs Scored
+rs_chart_data = filtered_data[['Team', 'RS']]
 fig, ax = plt.subplots(figsize=(10, 7))
 ax.bar(rs_chart_data['Team'], rs_chart_data['RS'])
 ax.set_title(f'Runs Scored by Teams in {selected_year}')
@@ -22,11 +19,16 @@ ax.set_xlabel('Teams')
 ax.set_ylabel('Runs Scored')
 plt.xticks(rotation=45)
 plt.tight_layout()
-st.pyplot(fig)  # Pass the figure object directly
+st.pyplot(fig)
+print_text_one = """
+Offensive Indicator: RS is a direct measure of a team's offensive capability. A higher number of runs scored indicates a strong offensive team. By tracking and comparing RS across teams or over time, one can gauge the offensive strength and efficiency of different teams.
 
+Winning Probability: Typically, teams that score more runs have a higher likelihood of winning games. Analyzing RS can provide insights into a team’s winning potential.
+"""
+st.markdown(print_text_one)
 
-# Create a bar chart for Runs Allowed
-ra_chart_data = filtered_data.sort_values('RA', ascending=False)
+# Runs Allowed
+ra_chart_data = filtered_data[['Team', 'RA']]
 fig, ax = plt.subplots(figsize=(10, 7))
 ax.bar(ra_chart_data['Team'], ra_chart_data['RA'], color='red')
 ax.set_title(f'Runs Allowed by Teams in {selected_year}')
@@ -34,35 +36,27 @@ ax.set_xlabel('Teams')
 ax.set_ylabel('Runs Allowed')
 plt.xticks(rotation=45)
 plt.tight_layout()
-st.pyplot(fig)  # Pass the figure object directly
+st.pyplot(fig)
 
-# Create a bar chart for Runs Allowed
-playoff_chart_data = filtered_data.sort_values('Playoffs')
+print_text_two = """
+Defensive Indicator: RA reflects the effectiveness of a team's defense and pitching staff. A lower number of runs allowed suggests a strong defensive team. Comparing RA across teams or over time can help evaluate the defensive prowess of teams.
+
+Team Improvement: By analyzing the RA metric, teams can identify areas of improvement in their defense to decrease the number of runs allowed, which in turn could lead to more wins.
+"""
+st.markdown(print_text_two)
+
+# Playoffs
+playoff_chart_data = filtered_data[['Team', 'Playoffs']]
 fig, ax = plt.subplots(figsize=(10, 7))
-ax.bar(ra_chart_data['Team'], ra_chart_data['RA'], color='red')
-ax.set_title(f'Runs Allowed by Teams in {selected_year}')
+ax.bar(playoff_chart_data['Team'], playoff_chart_data['Playoffs'], color='green')
+ax.set_title(f'Made It To The Playoffs by Teams in {selected_year}')
 ax.set_xlabel('Teams')
-ax.set_ylabel('Runs Allowed')
+ax.set_ylabel('Playoffs')
 plt.xticks(rotation=45)
 plt.tight_layout()
-st.pyplot(fig)  # Pass the figure object directly
-
-
-
-
-
-
-
-
-
-
-# # Create the stacked bar chart
-# ax = plot_data.plot(kind='bar', stacked=True, figsize=(10, 7))
-
-# # Labeling
-# ax.set_ylabel('Runs')
-# ax.set_title('Runs Scored and Runs Allowed per Team')
-
-# plt.xticks(rotation=45)  # Rotates X-axis labels for better readability
-
-# st.write(ax.figure)
+st.pyplot(fig)
+print_text_three = """
+Importance: Making it to the playoffs is a significant achievement, indicating that a team performed well during the regular season and is among the top contenders for the championship.
+Use: The playoff indicator provides a binary outcome, allowing for a clear distinction between successful and non-successful seasons.
+"""
+st.markdown(print_text_three)
