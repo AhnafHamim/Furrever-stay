fe2c637c70c2aaf7abbf3043ab71448b082aeb1e
Ubaydul Haque Sami <64384070+samihaque@users.noreply.github.com>
2023-11-29 01:40:52 -0500

    Corrected age for long-beach dataset, improved f1 score of the 'more or less than a month' model.fixed the streamlit app problem, now user interface is working fine

--- new_streamlit_app/app.py
+++ new_streamlit_app/app.py
@@ -5,47 +5,45 @@ import pandas as pd
 # Load your trained model
 model = load("use_model.joblib")
 
-
 def main():
-    st.title("Cat Attributes Prediction")
+    st.title("Cat Shelter staying Prediction")
 
     # User inputs through dropdown menus
-    color_mapping = {"BLACK": 0, "GRAY": 1, "OTHER": 2, "TABBY": 3, "WHITE": 4}
+    color_mapping = {"BLACK": 0, "GRAY": 1, "TABBY": 3, "WHITE": 4, "OTHER": 2}
     condition_mapping = {
         "ILL MILD": 0,
         "ILL SEVERE": 1,
         "INJURED": 2,
         "NORMAL": 3,
-        "OTHER": 4,
         "UNDER WEIGHT": 5,
+        "OTHER": 4,
     }
-    sex_mapping = {0: "FEMALE", 1: "MALE"}
-    type_mapping = {0: "OTHER", 1: "OWNER SURRENDER", 2: "STRAY"}
 
     color = st.selectbox("Color", options=list(color_mapping.keys()))
     condition = st.selectbox("Condition", options=list(condition_mapping.keys()))
-    sex = st.selectbox("Sex", options=list(sex_mapping.values()))
-    type_ = st.selectbox("Type", options=list(type_mapping.values()))
+
+    # Use a slider for intake_age
+    intake_age = st.slider("Intake Age", min_value=0, max_value=20, value=1)
 
     if st.button("Predict"):
         # Prepare the input for the model
         input_data = pd.DataFrame(
             {
                 "simplified_color_encoded": [color_mapping[color]],
-                "simplified_sex_encoded": [0 if sex == "FEMALE" else 1],
                 "simplified_condition_encoded": [condition_mapping[condition]],
-                "simplified_type_encoded": [
-                    key for key, value in type_mapping.items() if value == type_
-                ][0],
+                "intake_age": [intake_age],
             }
         )
 
         # Make the prediction
         prediction = model.predict(input_data)
 
-        # Display the prediction
-        st.success(f"The model predicts: {prediction[0]}")
+        # Map predictions to human-readable labels
+        prediction_labels = {0: 'staying less than a month', 1: 'staying more than a month'}
+        human_readable_prediction = prediction_labels.get(prediction[0], 'Unknown')
 
+        # Display the prediction
+        st.success(f"The model predicts: {human_readable_prediction}")
 
 if __name__ == "__main__":
     main()
