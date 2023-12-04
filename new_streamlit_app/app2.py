0bad4f46532b2dbd67b834f8a2f4d5ee0250d006
AhnafHamim <hamimnycusa@gmail.com>
2023-12-04 05:21:21 -0500

    Fixed Quantitative prediction model

--- new_streamlit_app/app2.py
+++ new_streamlit_app/app2.py
@@ -10,16 +10,9 @@ def main():
     st.title("Cat Shelter Staying Quantitative Prediction")
 
     # User inputs through dropdown menus
-    color_mapping = {"BLACK": 0, "GRAY": 1, "TABBY": 3, "WHITE": 4, "OTHER": 2}
-    condition_mapping = {
-        "ILL MILD": 0,
-        "ILL SEVERE": 1,
-        "INJURED": 2,
-        "NORMAL": 3,
-        "UNDER WEIGHT": 5,
-        "OTHER": 4,
-    }
-    type_mapping = {0: 'OTHER', 1: 'OWNER SURRENDER', 2: 'STRAY'}
+    color_mapping = {'BLACK': 0, 'BLACK_N_WHITE': 1, 'BRN_TABBY': 2, 'GRAY': 3, 'GRAY_N_WHITE': 4, 'GRAY_TABBY': 5, 'MIX': 6, 'ORG_TABBY': 7, 'OTHER': 8, 'OTHER_TABBY': 9, 'TORTIE': 10}
+    condition_mapping = {'HEALTHY': 0, 'ILL': 1, 'OTHER': 2}    
+    type_mapping = {'OTHER': 0, 'OWNER SURRENDER': 1, 'STRAY': 2}
 
     color = st.selectbox("Color", options=list(color_mapping.keys()))
     type = st.selectbox("Type", options=list(type_mapping.keys()))
@@ -27,35 +20,28 @@ def main():
 
     # Use a slider for intake_age
     intake_age = st.slider("Intake Age", min_value=0, max_value=20, value=1)
-
+    
     if st.button("Predict"):
-        # Prepare the input for the model
+            # Prepare the input for the model
         input_data = pd.DataFrame(
             {
-                
+                "intake_age": [intake_age],
                 "simplified_condition_encoded": [condition_mapping[condition]],
                 "simplified_type_encoded":[type_mapping[type]],
-                "intake_age": [intake_age],
                 "simplified_color_encoded": [color_mapping[color]]
             }
         )
 
-        # Make the prediction
+            # Make the prediction
         prediction = model.predict(input_data)
-
-        # Map predictions to human-readable labels
-        prediction_labels = {
-            0: "staying less than a month",
-            1: "staying more than a month",
-        }
-        human_readable_prediction = prediction_labels.get(prediction[0], "Unknown")
+        predicted_days=round(prediction[0])
 
         # Determine the color based on the prediction
-        text_color = "red" if prediction[0] == 1 else "green"
+        text_color = "red" if prediction[0] >=30 else "green"
 
         # Display the prediction with colored text using Markdown syntax
         colored_text = (
-            f'<span style="color:{text_color}">{human_readable_prediction}</span>'
+            f'<span style="color:{text_color}">{predicted_days} days</span>'
         )
         st.markdown(f"The model predicts: {colored_text}", unsafe_allow_html=True)
 
